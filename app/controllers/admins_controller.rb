d0a474714c5f352403cca559a2d4d16eb5ad844b
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-07-20 17:17:21 -0300

    Parte1(Dashboard)

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,8 +1,9 @@
 class AdminsController < ApplicationController
 	before_action :authenticate_admin!
 
+	#Dashboard
 	def index
-		
+		@posts = Post.active
 	end
 
 	def show
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,17 +22,14 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.active = true
 
-    respond_to do |format|
       if @post.save
-        format.html { redirect_to dashboard_path, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        redirect_to dashboard_path, flash: { success: "Publicado com Sucesso"} 
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        redirect_to new_post_path, flash: { danger: "Ocorreu Erro ao Publicar!"} 
       end
     end
-  end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
